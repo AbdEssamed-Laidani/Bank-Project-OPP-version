469f94962dd8b6d2461a64813505adc9b5fd0b0e
AbeEssamed Laidani <abdolaidani0656@gmail.com>
2025-11-20 22:50:48 +0100

    add Client List screen

---
+++ clsClientListScreen.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include "clsBankClient.h"
+#include "clsScreen.h"
+#include <iomanip>
+#include <vector>
+#include <string>
+class clsClientListScreen : protected clsScreen
+{
+private:
+	static void PrintClientDataLine(clsBankClient& ClientData)
+	{
+		std::cout << "\t" << "| " << std::left << std::setw(15) << ClientData.GetAccountNumber() << std::right << "| " << std::left << std::setw(25) << ClientData.GetFullName() << std::right << "| " << std::left << std::setw(15) << ClientData.GetPhone() << std::right << "| " << std::left << std::setw(30) << ClientData.GetEmail() << std::right << "| " << std::left << std::setw(14) << ClientData.GetPinCode() << std::right << " |" << std::left << std::setw(15) << ClientData.GetBalance() << std::right << "|" << std::endl;
+
+	}
+
+public:
+
+	static void ShowClientList()
+	{
+		std::vector <clsBankClient> vClient = clsBankClient::GetClientsList();
+		clsScreen::_DrawHeaderScreen("Client List", "(" + std::to_string((int)vClient.size()) + ") Client(s).");
+		std::cout << "\t-------------------------------------------------------------------------------------------------------------------------------" << std::endl;
+		std::cout << "\t|" << std::left << std::setw(15) << " Account Number" << std::right << " |" << std::left << std::setw(25) << " Client Name" << std::right << " |" << std::left << std::setw(15) << " Phone" << std::right << " |" << std::left << std::setw(30) << " Email" << std::right << " |" << std::left << std::setw(15) << " Pin code" << std::right << " |" << std::left << std::setw(15) << " Balance" << std::right << "|" << std::endl;
+		std::cout << "\t-------------------------------------------------------------------------------------------------------------------------------" << std::endl;
+		for (clsBankClient& C : vClient)
+		{
+			PrintClientDataLine(C);
+		}
+		std::cout << "\t-------------------------------------------------------------------------------------------------------------------------------" << std::endl;
+	}
+};
+
