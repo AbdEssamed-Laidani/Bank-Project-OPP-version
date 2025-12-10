e418591890072988646b1aaa126468cc8031e840
AbdEssamed Laidani lap <abdolaidani0656@gmail.com>
2025-12-10 22:48:08 +0100

    Add User manage screen

---
+++ clsUserManageScreen.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "clsInputValidate.h"
+#include "clsBankClient.h"
+#include "clsScreen.h"
+#include <iomanip>
+class clsUserManageScreen : protected clsScreen
+{
+private:
+    enum enManageUserMenu 
+    { UsersList = 1, AddUser, DelteUser, UpdateUser, FindUser, MainMenu };
+	
+    static short _ReadOption()
+    {
+        return clsInputValidate::ReadPositiveNumberInRange("Choose an option [1-6]: ", 1, 6);
+    }
+    static void _GoBackToMainScreen()
+    {
+        std::cout << "Press any key to go back to main screen...";
+        system("pause>0");
+        ShowManageUserScreen();
+    }
+    static void _ShowUsersListScreen()
+    {
+        std::cout << "Users list screen will be here...\n";
+    }
+    static void _ShowFindUserScreen()
+    {
+        std::cout << "Find user screen will be here...\n";
+    }
+    static void _ShowDeleteUserScreen()
+    {
+        std::cout << "Delete user screen will be here...\n";
+    }
+    static void _ShowUpdateUserScreen()
+    {
+        std::cout << "Update user screen will be here...\n";
+    }
+    static void _ShowAddUserScreen()
+    {
+        std::cout << "Add user screen will be here...\n";
+    }
+    
+    static void _PerformManageUsersOption(enManageUserMenu Option)
+    {
+        switch (Option)
+        {
+        case enManageUserMenu::UsersList:
+            clsScreen::_ClearScreen();
+            _ShowUsersListScreen();
+            _GoBackToMainScreen();
+            break;
+        case enManageUserMenu::AddUser:
+            clsScreen::_ClearScreen();
+            _ShowAddUserScreen();
+            _GoBackToMainScreen();
+                break;
+        case enManageUserMenu::DelteUser:
+            clsScreen::_ClearScreen();
+            _ShowDeleteUserScreen();
+            _GoBackToMainScreen();
+                break;
+        case enManageUserMenu::FindUser:
+            clsScreen::_ClearScreen();
+            _ShowFindUserScreen();
+            _GoBackToMainScreen();
+                break;
+        case enManageUserMenu::UpdateUser:
+            clsScreen::_ClearScreen();
+            _ShowUpdateUserScreen();
+            _GoBackToMainScreen();
+                break;
+        case enManageUserMenu::MainMenu:
+            break;
+        default:
+            break;
+        }
+    }
+public:
+
+
+
+	static void ShowManageUserScreen()
+	{
+		std::string Title = "\t     Manage Users";
+		clsScreen::_ClearScreen();
+		clsScreen::_DrawHeaderScreen(Title);
+        std::cout << "\t\t\t\t\t======================================\n";
+        std::cout << "\t\t\t\t\t           Manage Users Menu\n";
+        std::cout << "\t\t\t\t\t======================================\n\n";
+        std::cout << "\t\t\t\t\t[1] List Users.\n";
+        std::cout << "\t\t\t\t\t[2] Add New User.\n";
+        std::cout << "\t\t\t\t\t[3] Delete User.\n";
+        std::cout << "\t\t\t\t\t[4] Update User.\n";
+        std::cout << "\t\t\t\t\t[5] Find User.\n";
+        std::cout << "\t\t\t\t\t[6] Main Menu.\n";
+        std::cout << "\n\t\t\t\t\t======================================\n";
+        std::cout << "\t\t\t\t\t"; _PerformManageUsersOption((enManageUserMenu)_ReadOption());
+	}
+
+};
+
